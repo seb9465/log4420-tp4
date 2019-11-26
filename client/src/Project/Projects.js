b51a7c76e97f418f7bf1ef71fe4a3e0eefcc6312
Sebastien Cadorette <cado.94@me.com>
2019-11-25 20:58:36 -0500

    `projects` page half done

--- client/src/Project/Projects.js
+++ client/src/Project/Projects.js
@@ -31,22 +31,41 @@ const ProjectDescription = props => {
 }
 
 export default () => {
-
-  const loading = false
-  const projects = []
-
   // À COMPLÉTER
   // 1- Récupérer les projets du service web http://localhost:3000/api/projects avec 'fetch' et avec l'entête 'accept-language' à 'fr'.
   // 2- Une fois que les données ont été récupérées, le loading devient false
   // 3- Vous remarquerez qu'on duplique la description de chaque projet dans le Pug.
   //    Évitez la duplication en créant une nouvelle composante React et insérez la dans le Pug ci-bas.
 
-  const currentProjects = projects
-    .filter(p => p.current)
-    .sort((p1, p2) => p1.year < p2.year ? 1 : p1.year > p2.year ? -1 : 0)
-  const pastProjects = projects
-    .filter(p => !p.current)
-    .sort((p1, p2) => p1.year < p2.year ? 1 : p1.year > p2.year ? -1 : 0)
+  const [loading, setLoading] = useState(true);
+  const [currentProjects, setCurrentProjects] = useState([]);
+  const [pastProjects, setPastProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const options = {
+        headers: { 'accept-language' : 'fr' }
+      }
+
+      const response = await fetch('http://localhost:3000/api/projects', options);
+
+      const proj = await response.json();
+
+      return proj;
+    }
+
+    fetchProjects().then(projects => {
+    setCurrentProjects(
+      projects
+        .filter(p => p.current)
+        .sort((p1, p2) => p1.year < p2.year ? 1 : p1.year > p2.year ? -1 : 0));
+    setPastProjects(
+      projects
+        .filter(p => !p.current)
+        .sort((p1, p2) => p1.year < p2.year ? 1 : p1.year > p2.year ? -1 : 0));
+	  setLoading(false);
+    })
+  }, [])
 
   return pug`
     .loading-container
